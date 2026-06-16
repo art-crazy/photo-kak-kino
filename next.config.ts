7c41a48e1f52b0e17edca5577c08aa698dc55254
arzhannikov <artcrazh.mail@gmail.com>
2026-06-16 12:36:18 +0300

    Optimize site images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60 * 60 * 24 * 30,
+  },
   async redirects() {
     return [
       {
